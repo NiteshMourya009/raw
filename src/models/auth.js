ae971b3352237f8eccaecfbaeab4365e8c4c2158
Sheeegits <Singhsheetal763@gmail.com>
2025-05-29 07:23:53 +0530

    Updated code

---
+++ src/models/auth.js
@@ -0,0 +1,18 @@
+// src/models/auth.js
+import mongoose from 'mongoose';
+
+const authSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+const Auth = mongoose.model('Auth', authSchema);
+
+export default Auth;
